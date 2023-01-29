128331e7726c646029bb79eccdf0b5fbc3700206
Mirabbosbek9778 <jwicj969@gmail.com>
2023-01-30 01:36:35 +0500

    homework type conversion

---
+++ type conversion/index.js
@@ -0,0 +1,65 @@
+// let a = 1 ,b = 1;
+
+// let c = ++a;
+// console.log(c);  //2
+
+// let d = b++;
+
+// // console.log(d);//1
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+
+// console.log(x);  ///javobi: 2
+
+// console.log("" + 1 + 0);  ///10
+
+// console.log("" - 1 + 0);  //-1
+
+// console.log(true+false);  ///1
+
+// console.log(6/"3"); //2
+
+// console.log("2" * "3"); //6
+
+// console.log(4 + 5 + "px"); //9px
+
+// console.log("$" + 4 + 5); //45$
+
+// console.log("4" - 2);   ///2
+
+// console.log("4px" - 2);  //NaN
+
+// console.log(" -9 " + 5); //-9  5
+
+// console.log(" -9 " - 5); // -14
+
+// console.log(null + 1);  //1
+
+// console.log(undefined + 1);  //NaN
+
+// console.log(" \t \n" - 2);  //-2
+
+// console.log(5 > 4); ///true
+
+// console.log("apple">"pineapple");  ///false
+
+// console.log("2" > "12"); //true
+
+// console.log(undefined === null);  ///false
+
+// console.log(undefined == null);  //true
+
+// console.log(null == "\n0\n");  //false
+
+// console.log(null === +"\n0\n");  //false
+
+
+//////////////////////////
+
+
+if("0"){
+    console.log('hello');
+}
+    // |||
+/////////hello
