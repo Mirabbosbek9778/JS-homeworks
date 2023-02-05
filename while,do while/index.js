5d48756c85d575ab47e8fa384caa31db4dac3af8
Mirabbosbek9778 <jwicj969@gmail.com>
2023-02-05 18:22:52 +0500

    While,function

---
+++ while,do while/index.js
@@ -0,0 +1,52 @@
+// let i = 0;
+
+// for (; i < 3; i++) {
+//   console.log(`number ${i}`);
+// }
+///////////////////number 0,number 1 ,number 2 deb chiqadi chunki oshib borish tartibi
+
+//////////////////
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     console.log(true);
+//   } else {
+//     console.log("Did parents allow you?");
+//   }
+// }
+// checkAge(18);
+
+//////bunda funksiyada 18dan katta bosa true deb chiqsin agar 18ddan kichik bosa else holat chiqsin
+
+///////////////////////////////////////////
+
+// console.log(Math.min(2, 5) == 2); ////true
+// console.log(Math.min(3, -1) == -1); ////true
+// console.log(Math.min(1, 1) == 1); ////true
+
+////////////////////////////////
+
+// console.log(Math.pow(3, 2));////daraja bolyapti 9
+// console.log(Math.pow(3, 3));////daraja bolyapti 27
+// console.log(Math.pow(1, 100));////daraja bolyapti 1
+
+/////////////////////
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) {
+//     yes();
+//   } else {
+//     no();
+//   }
+// }
+
+// ask(
+//   "Do you agree?",
+//   function () {
+//     console.log("you agreed.");
+//   },
+//   function () {
+//     console.log("you canceled the execution.");
+//   }
+// );
+// console.log(ask("hey", 1, 2));
