b8eea4949a1fdfcf0546bf4f5eeca39f1055b05c
Mirabbosbek9778 <jwicj969@gmail.com>
2023-02-06 18:55:02 +0500

    Number,Math.pow,sqrt

---
+++ Number,All you need/index.js
@@ -0,0 +1,81 @@
+// let obj = { name: "test", child: { name: "Mirabbos" } };
+
+// console.log(Object.keys(obj));
+
+///////////////////////
+
+// console.log(100_000_000+1);
+
+// console.log(1e500);///infinitga tushadi
+
+// console.log(0.1 + 0.2 === 0.3);
+
+// console.log(1/3);
+
+// let num = 0.1 + 0.2;
+
+// console.log(Number(num.toFixed(1))); ///+1,number
+
+/////////////////
+
+// let num = "0.1 + 0.2";
+
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+// console.log(Number(num));
+// console.log(eval(num));
+
+// console.log(Number.parseInt(num));
+// console.log(Number.parseFloat(num));
+
+// console.log(+"1_00" + 1);  ///NaN
+
+// let num = 5;
+
+// console.log(num.toString(2));
+
+// console.log(toString(num));object undefined
+
+// let num = 5;
+
+// console.log(num.toString(16));
+// console.log(parseInt("5", 16));
+
+// console.log(isNaN(+"1_000"));
+
+// let res = Object.is("web", "webb");
+
+// console.log(res);
+
+// let num = 5.1234457;
+// let num1 = 0.00001217343;
+
+// console.log(num.toPrecision(3));
+// console.log(num1.toPrecision(3));
+
+// console.log(9_999_999_999_999_999_999);
+
+// console.log(Math.abs(-10));
+
+// console.log(Math.round(2.5));///3
+// console.log(Math.round(2.6));///3
+// console.log(Math.round(2.4));///2
+
+// console.log(Math.floor(2.999));  ///2
+// console.log(Math.floor(10.001)); ///10 ozak qismini oberadi
+
+// console.log(Math.ceil(10.01)); ///10 ozak qismini oberadi
+
+// console.log(Math.trunc(10.999));
+
+// console.log(Math.random()*10);
+
+// console.log(Math.min(1, 2, 3, 4, 5, 6, 8, 0.1));
+
+// console.log(Math.sign(-0));
+// console.log(Math.sign(0));
+// console.log(Math.sign(123));
+// console.log(Math.sign(-123));
+// console.log(Math.sign('+123'));
+// console.log(Math.sign('123'));
+// console.log(Math.sign(123e3));
