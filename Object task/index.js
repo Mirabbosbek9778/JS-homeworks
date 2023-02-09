d16252eb4f0c1cb7c45b1f69b49845cb5b723f69
Mirabbosbek9778 <jwicj969@gmail.com>
2023-02-09 16:44:00 +0500

    Object hometask

---
+++ Object task/index.js
@@ -0,0 +1,72 @@
+///////////////////home task///////////////////////////
+//1)bo'sh ob'ekt foydalanuvchisini yaratish
+
+// let user = {};
+
+// console.log(user);
+
+////////////////////////////////
+
+// 2)namega john bering?
+
+// let user = {
+//   name: "John",
+// };
+
+// // console.log(user);  ////{ name: 'John' }
+
+/////////////////////////////
+
+// 3)Surnamega Smith qohing objectga
+
+// let user = {
+//   name: "John",
+//   surname: "Smith",
+// };
+// // console.log(user);//{ name: 'John', surname: 'Smith' }
+
+////////////////////////////////////
+
+// 4)namega pete qoshing  ///Name:'Pete'
+
+// let user = {
+//   name: "John",
+//   surname: "Smith",
+// };
+// console.log({ ...user, name: "Pete" });
+
+//////////////////////////////////////////
+
+// let isEmpty = "hey";
+
+// let schedule = {
+//   isEmpty() {},
+// };
+
+// console.log(isEmpty(schedule));
+
+// schedule["8:30"] = "get up";
+
+// console.log(isEmpty(schedule));
+
+///////////////////////////////////////
+
+// let saralies = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// ///bu yigindi ochilgan ozgaruvchi qiymati doimo nolga teng bolishi kerak chunki 1 bosa 1ta oshib ketadi!
+
+// let sum = 0;
+
+// for (let key in saralies) {
+//   if (saralies.hasOwnProperty(key)) {
+//     sum += saralies[key];
+//   }
+// }
+
+// console.log(sum); //javobi 390
+
+// // ////////////////////////////
