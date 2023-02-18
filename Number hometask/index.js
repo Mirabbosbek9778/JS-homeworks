a93a7e9aab7f352b56fa665398eb1818dfb97380
Mirabbosbek9778 <jwicj969@gmail.com>
2023-02-18 22:20:43 +0500

    Number task

---
+++ Number hometask/index.js
@@ -0,0 +1,22 @@
+// //////////////
+
+// let a = "webbrain";
+// let b = 10;
+
+// for (let i = 0; i <= b; i++) {
+//   console.log(a);
+// }
+////////////////////////////////
+
+// 1)istalgan sonni binary korinishiga otkazish (nechta usuli bor)
+
+// let n = 5;
+
+// console.log(n.toString(2));////ikkilik sanoq sistemasiga otdi
+// console.log(n.toString(4));////4lik sanoq sistemasiga otdi
+// console.log(n.toString(8));////8lik sanoq sistemasiga otdi
+// console.log(n.toString(16));////16lik sanoq sistemasiga otdi
+
+///////////////////////
+
+
